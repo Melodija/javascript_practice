4f19ed586ea4f5dce4d5d90c8128e44d3da1fa67
Emmett Walsh <emmett.finarr.walsh@gmail.com>
2016-05-11 09:45:29 +0100

    First commit for day-two, no changes

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -4,7 +4,7 @@ describe('Javabuzz', function() {
 
   describe('knows when a number is', function() {
 
-      it('divisible by 3', function(){
+      it('divisible by 3', function() {
         javabuzz = new Javabuzz();
         expect(isDivisibleByThree(3)).toBe(true);
       });
