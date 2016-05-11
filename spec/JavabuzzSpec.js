f1085013bceee4e72aa79e785eac2314f92cec13
Ruta <ruta@baltiejus.com>
2016-05-11 10:01:27 +0100

    not divisible by 5 failing

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -20,6 +20,10 @@ describe('Javabuzz', function() {
     it('divisible by 3', function() {
       expect(javabuzz.isDivisibleByThree(1)).toBe(false);
     });
+
+    it('divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
+    });
   });
 
 });
